6ff586527aa4653519afc2edcc8901d022340f92
MOHAMMAD ASIF <asifmohd1146@gmail.com>
2023-11-30 19:29:20 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,10 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(cors(
+  origin: 'https://tasklist-mern-frontend.vercel.app/',
+  optionsSuccessStatus: 200
+));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
