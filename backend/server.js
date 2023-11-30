f7e8dd3b136a43cdc6061ead6d5dc377d4240748
MOHAMMAD ASIF <asifmohd1146@gmail.com>
2023-11-30 20:49:47 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -22,12 +22,13 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/", (req, res) => {
-  res.send("hello");
-});
+
 
 app.use("/api/goals", require("./routes/goalRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
+app.use("/", (req, res) => {
+  res.send("hello");
+});
 
 // Serve frontend
 // if (process.env.NODE_ENV === "production") {
